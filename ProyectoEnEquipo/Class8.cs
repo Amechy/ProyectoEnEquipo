4e5bba91f2be91fbbbdd60e5e3fe2ec1283366cd
Adrian Muñoz Mudarra <eladrixhd@gmail.com>
2017-04-06 14:22:28 +0200

    Añadiendo la Class8

---
+++ ProyectoEnEquipo/Class8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEnEquipo
+{
+    class Class8
+    {
+        public string nombre = "";
+
+        public Class8()
+        {
+            this.nombre = "Adrián (ElAdrixHD) Muñoz Mudarra";
+        }
+    }
+}
