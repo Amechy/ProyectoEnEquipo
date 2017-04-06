7e815e6109cc6b380c65fabda5093919d223343c
Alejandro Mechine <alejandromechinegamarra@gmail.com>
2017-04-06 14:20:24 +0200

    Creando una clase con nombre

---
+++ ProyectoEnEquipo/Clase00.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEnEquipo
+{
+    class Clase00
+    {
+        public string nombre;
+        public Clase00()
+        {
+            this.nombre = "Alejandro Mechiné Gamarra";
+        }
+    }
+}
