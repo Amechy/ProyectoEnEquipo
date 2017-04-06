7bb06e04a9a8f7e366522edc0c222f35de0baf8d
Javier Amado <JavierAmado7@gmail.com>
2017-04-06 14:21:07 +0200

    Añadiendo clase11

---
+++ ProyectoEnEquipo/clase11.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEnEquipo
+{
+    class clase11
+    {
+        public string nombre;
+        public clase11()
+        {
+            this.nombre = "Cristóbal Javier Miguel Amado";
+        }
+    }
+}
