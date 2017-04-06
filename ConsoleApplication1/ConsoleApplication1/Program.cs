879914441f27598a50571bf598841ce4112b91de
alejandroagca <alejandroagca1995@gmail.com>
2017-04-06 14:30:21 +0200

    Primer Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Clase06
+    {
+        public string nombre;
+        public Clase06() 
+        {
+            this.nombre = "Alejandro Aguilar";
+        }
+    }
+}
