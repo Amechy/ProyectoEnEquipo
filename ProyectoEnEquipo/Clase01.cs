ef7b97c436c922859305a56d1b6c3e832004d204
Carlos Cruz <carloscruzdominguez@gmail.com>
2017-04-06 14:22:34 +0200

    Añado la Clase01.cs

---
+++ ProyectoEnEquipo/Clase01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEnEquipo
+{
+    class Clase01
+    {
+        public string nombre;
+
+        public Clase01()
+        {
+            this.nombre = "Carlos";
+        }
+    }
+}
